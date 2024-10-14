4fba1bef72f311b607fa30fb97e39c1355780f6c
MMrudula <mrudula.vm@gmail.com>
2024-10-14 15:09:45 -0700

    Update Passwordgenerator.py

--- Passwordgenerator.py
+++ Passwordgenerator.py
@@ -1,41 +1,4 @@
-# import streamlit as st # calling streamlit interfacec
-# import random
-# import string
-# def generate_password(length,use_digits,use_special_chars,use_uppercase): #declaring attributes
-#     characters = string.ascii_lowercase
-#     if use_digits:
-#         characters += string.digits
-#     if use_special_chars:
-#         characters += string.punctuation
-#     if use_uppercase:
-#         characters += string.ascii_uppercase
-#     password = ''.join(random.choice(characters) for _ in range(length))
-#     return password
-# generate_password(10,True,True,False)
-# print(generate_password(10,True,True,True))
-# #user preferences 
-# st.title('Secure Password Generator')
-# st.sidebar.header('Password Options')
-# length = st.sidebar.slider('Password Length', min_value=6, max_value=20, value=12, step=1)
-# use_digits = st.sidebar.checkbox('Include Digits', value=True)
-# use_special_chars = st.sidebar.checkbox('Include Special Characters', value=True)
-# use_uppercase = st.sidebar.checkbox('Include Uppercase Letters', value=True)
-# st.title('Secure Password Generator')
-# st.sidebar.header('Password Options')
-# length = st.sidebar.slider('Password Length', min_value=6, max_value=20, value=12, step=1)
-# use_digits = st.sidebar.checkbox('Include Digits', value=True)
-# use_special_chars = st.sidebar.checkbox('Include Special Characters', value=True)
-# use_uppercase = st.sidebar.checkbox('Include Uppercase Letters', value=True)
-# if st.button('Generate Password'):
-#     if length < 6:
-#         st.error('Password length should be at least 6 characters.')
-# else:
-#        generated_password = generate_password(length, use_digits, use_special_chars, use_uppercase)
-#        st.success('Generated Password:')
-#        st.code(generated_password)
 
-#submit in github 
-#figma  figma.com  what the website looks like 
 import streamlit as st
 import random
 import string
@@ -69,10 +32,3 @@ print("hello Team")
 
 
 
-# Add addtional features :  created by name  , profile , pics names, footer - "  " , colors 
-
-# git add -A → Adds all files to your commit 
-# git commit -m ‘commit message’ → Creates a commit message to send to github
-# git push → Pushes the commit message and code to github 
-# ns: Add a feature to notify users of upcoming deadlines.
-#ex: notification banner 
